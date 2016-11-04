fda55ed6244b80f1d31545bd4ba8e98fca4dc7a3
Nick Johnson <nick@sabramedia.com>
2016-11-04 16:06:55 -0400

    Customer - addTrial method.

--- Pigeon/Customer.php
+++ Pigeon/Customer.php
@@ -3,18 +3,38 @@
 class Pigeon_Customer extends Pigeon
 {
 
+	public $customer_id = NULL;
+
+	public function isLoaded()
+	{
+		if( $this->customer_id ){
+			return TRUE;
+		}else{
+			return FALSE;
+		}
+	}
+
 	public function create( $input, $force_auth = FALSE )
 	{
 		if( !array_key_exists("email", $input) ){
 			throw new Exception("An email is required for account creation");
 		}
 
-		return $this->post("/customer", array("data"=>$input,"force_auth"=>$force_auth));
+		$response = $this->post("/customer", array("data"=>$input,"force_auth"=>$force_auth));
+		if( isset($response->customer->id) )
+			$this->customer_id = $response->customer->id;
+
+		return $response;
 	}
 
 	public function find( $customer_id )
 	{
-		return parent::get("/customer", array("id"=>$customer_id));
+		$response = parent::get("/customer", array("id"=>$customer_id));
+
+		if( isset($response->customer->id) )
+			$this->customer_id = $response->customer->id;
+
+		return $response;
 	}
 
 	public function search( $filters )
@@ -39,14 +59,22 @@ class Pigeon_Customer extends Pigeon
 
 	public function login( $email, $password )
 	{
-		return $this->post("/customer/login", array("email"=>$email,"password"=>$password));
+		$response = $this->post("/customer/login", array("email"=>$email,"password"=>$password));
+		if( isset($response->customer->id) )
+			$this->customer_id = $response->customer->id;
+
+		return $response;
 	}
 
 	public function sessionLogin( $customer_id )
 	{
 		$unique_session = md5(Pigeon_Configuration::get("pigeon_domain"));
 		if( array_key_exists($unique_session."_id", $_COOKIE) && array_key_exists($unique_session."_hash", $_COOKIE) ){
-			return $this->post("/customer/session_login", array("customer_id"=>$customer_id,"session_id"=>$_COOKIE[$unique_session."_id"],"session_hash"=>$_COOKIE[$unique_session."_hash"]));
+			$response = $this->post("/customer/session_login", array("customer_id"=>$customer_id,"session_id"=>$_COOKIE[$unique_session."_id"],"session_hash"=>$_COOKIE[$unique_session."_hash"]));
+			if( isset($response->customer->id) )
+				$this->customer_id = $response->customer->id;
+
+			return $response;
 		}else{
 			return FALSE;
 		}
@@ -56,6 +84,7 @@ class Pigeon_Customer extends Pigeon
 	{
 		$unique_session = md5(Pigeon_Configuration::get("pigeon_domain"));
 		if( array_key_exists($unique_session."_id", $_COOKIE) && array_key_exists($unique_session."_hash", $_COOKIE) ){
+			$this->customer_id = NULL;
 			return $this->post("/customer/session_logout", array("customer_id"=>$customer_id,"session_id"=>$_COOKIE[$unique_session."_id"],"session_hash"=>$_COOKIE[$unique_session."_hash"]));
 		}else{
 			return FALSE;
@@ -72,11 +101,17 @@ class Pigeon_Customer extends Pigeon
 	 */
 	public function logout( $id_or_token, $type = "token" )
 	{
+		$this->customer_id = NULL;
 		return $this->post("/customer/logout", array("token"=>$id_or_token,"type"=>$type));
 	}
 
 	public function getSSOLink( $customer_id, $url )
 	{
 		return "https://" . Pigeon_Configuration::get("pigeon_domain")."?psso=".$customer_id."&rd=".urlencode($url);
+	}
+
+	public function addTrial( $customer_id, $plan_number, $trial_days )
+	{
+		return $this->post("/customer/add_plan", array("customer_id"=>$customer_id,"plan_number"=>$plan_number, "plan_type"=>Pigeon_Plan::ITEM_TYPE_SUBSCRIPTION_TRIAL,"trial_days"=>$trial_days));
 	}
 }
